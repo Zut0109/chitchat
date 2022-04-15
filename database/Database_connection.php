d30f92334e0873447cf1f729c2245c091c41a957
Zut0109 <hoainguyen.010901@gmail.com>
2022-04-16 04:23:48 +0700

    Update Database_connection.php

--- database/Database_connection.php
+++ database/Database_connection.php
@@ -6,8 +6,7 @@ class Database_connection
 {
 	function connect()
 	{
-		$connect = new PDO("mysql:host=localhost; dbname=nhantin", "root", "");
-
+		$connect = new PDO("mysql:host=us-cdbr-east-05.cleardb.net; dbname=heroku_15432f44b19325e", "b879315a711615", "a9caf152");
 		return $connect;
 	}
 }
